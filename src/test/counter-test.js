210660a9d43f56fdbb87bcf3b38c7b39e2104e73
derrick <djmulinde17@gmail.com>
2022-10-20 00:23:15 +0300

    Update source files

---
+++ src/test/counter-test.js
@@ -0,0 +1,9 @@
+const addLikes = require('../modules/addLikes.js');
+
+describe('test-likes', () => {
+  test('should ', () => {
+    // expect(addLikes()).to;
+    // Mock current number of likes
+    //
+  });
+});
