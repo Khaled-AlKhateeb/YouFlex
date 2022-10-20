ec5d6e6e5f09ae88b7882d7c5a4ff17ba00b6328
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-10-20 12:39:05 +0300

    create new elements and function tests

---
+++ src/modules/display-popup.js
@@ -0,0 +1,84 @@
+import commentsCounter from "./comments-counter.js";
+
+const closeBtn = document.getElementById('closeBtn');
+const commentPopup = document.getElementById('commentPopup');
+const main = document.querySelector('.main');
+const thumbnail = document.getElementById('thumbnail');
+const movieTitle = document.getElementById('movieTitle');
+const details = document.getElementById('details');
+const commentBtn = document.getElementById('commentBtn');
+const nameInput = document.getElementById('nameInput');
+const commentInput = document.getElementById('commentInput');
+const commentsList = document.getElementById('commentsList');
+const commentsDisplay = document.getElementById('commentsDisplay');
+let aquiredData = [];
+let commentsData = [];
+
+const getApi = async (movie) => {
+  const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${movie}`);
+  aquiredData = await response.json();
+  popupContent(aquiredData);
+}
+
+const getCommentsApi = async (id) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/CleVZYCORgbN3niQTjHc/comments?item_id=${id}`);
+  commentsData = await response.json();
+  commentsContent(commentsData);
+}
+
+export const display = (e) => {
+  commentPopup.style.display = 'flex';
+  main.style.display = 'none';
+  const movieName = e.target.parentNode.children[1].children[0].innerHTML;
+  getApi(movieName);
+}
+
+closeBtn.addEventListener('click', () => {
+  commentsList.innerHTML = '';
+  commentsDisplay.innerHTML = '';
+  commentPopup.style.display = 'none';
+  main.style.display = 'block';
+})
+
+export const popupContent = (movieData) => {
+  thumbnail.src = movieData.image.original;
+  movieTitle.innerHTML = movieData.name;
+  details.innerHTML = movieData.summary;
+  getCommentsApi(aquiredData.id);
+}
+
+export const commentsContent = (comment) => {
+  if (comment) {
+    comment.forEach(element => {
+    const commentItem = document.createElement('i');
+    commentItem.classList.add('comments-item');
+    commentItem.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
+      commentsList.appendChild(commentItem);
+    });
+    commentsCounter();
+  }
+}
+
+const sendData = async (data) => {
+  const postUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/CleVZYCORgbN3niQTjHc/comments';
+  const parameters = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+
+  await fetch(postUrl, parameters);
+}
+
+commentBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  sendData({
+    'item_id': aquiredData.id,
+    'username': nameInput.value,
+    'comment': commentInput.value
+  });
+  nameInput.value = '';
+  commentInput.value = '';
+});
