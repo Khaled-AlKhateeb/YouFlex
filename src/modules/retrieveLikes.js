87654f29ce2175239a89fc15fc853bb0c4eee45f
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-10-21 19:37:49 +0300

    remove file

--- src/modules/retrieveLikes.js
+++
@@ -1,8 +0,0 @@
-const retrieveLikes = async () => {
-  const retrieveLikesUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/CleVZYCORgbN3niQTjHc/likes';
-  const response = await fetch(retrieveLikesUrl);
-
-  return response.json();
-};
-retrieveLikes();
-export default retrieveLikes;
